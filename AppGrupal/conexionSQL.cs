6c76c72f506fcfdcbb640db940d2e070cd9bee38
Luketruke <lucaszabalett@gmail.com>
2022-04-24 14:52:47 -0300

    Agregado
    Se agrego la clase conexion al proyecto

---
+++ AppGrupal/conexionSQL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AppGrupal
+{
+    class conexionSQL
+    {
+        SqlConnection conexionDB = new SqlConnection();
+        SqlCommand comando = new SqlCommand();
+
+        string cadenaConexion = "server=localhost; database=Productos; integrated security=true;";
+
+        public SqlConnection abrirConexion()
+        {
+            try
+            {
+                conexionDB.ConnectionString = cadenaConexion;
+                conexionDB.Open();
+                //MessageBox.Show("Se establecio conexion con la DB");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo establecer conexion con la DB" + ex.Message);
+            }
+            return conexionDB;
+        }
+
+        public SqlConnection cerrarConexion()
+        {
+            try
+            {
+                conexionDB.ConnectionString = cadenaConexion;
+                conexionDB.Close();
+                //MessageBox.Show("Se cerro la conexion con la DB");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo cerrar la conexion con la DB" + ex.Message);
+            }
+            return conexionDB;
+        }
+    }
+}
